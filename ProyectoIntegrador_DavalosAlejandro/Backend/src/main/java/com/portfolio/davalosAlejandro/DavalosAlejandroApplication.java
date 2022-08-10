a4da50b1a7d73f213de801f34bf308df7d1df96c
Aledavalos2022 <agdavalos14@gmail.com>
2022-08-10 20:30:18 -0300

    backend

---
+++ ProyectoIntegrador_DavalosAlejandro/Backend/src/main/java/com/portfolio/davalosAlejandro/DavalosAlejandroApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.davalosAlejandro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavalosAlejandroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavalosAlejandroApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador_DavalosAlejandro/Backend/src/main/java/com/portfolio/davalosAlejandro/entity/Persona.java
@@ -0,0 +1,25 @@
+
+package com.portfolio.davalosAlejandro.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private Long id;
+    @NotNull
+    @Size(min = 1, max=50, message ="No cumple con la longitud")
+    private String nombre;
+    @NotNull
+    @Size(min = 1, max=50, message ="No cumple con la longitud")
+    private String apellido;
+    @Size(min = 1, max=50, message ="No cumple con la longitud")
+    private String img;
+}
