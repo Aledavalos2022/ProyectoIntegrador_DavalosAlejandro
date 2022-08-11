8154f3b2b411cddb75395dfdd71a84d5a57235a2
Aledavalos2022 <agdavalos14@gmail.com>
2022-08-11 19:05:55 -0300

    netbeans

---
+++ ProyectoIntegrador_DavalosAlejandro/Backend/src/main/java/com/portfolio/davalosAlejandro/Interface/IPersonaService.java
@@ -0,0 +1,15 @@
+
+package com.portfolio.davalosAlejandro.Interface;
+
+import com.portfolio.davalosAlejandro.entity.Persona;
+import java.util.List;
+
+
+
+
+public interface IPersonaService {
+    public List<Persona> getPersona();
+    public void savePersona (Persona persona);
+    public void deletePersona (Long id);
+    public Persona findPersona (Long id);
+}
