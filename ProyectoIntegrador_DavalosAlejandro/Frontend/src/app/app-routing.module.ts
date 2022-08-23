32a8f875550aa816f923b8a14b2f68acfcc4d49d
Aledavalos2022 <agdavalos14@gmail.com>
2022-08-23 20:21:40 -0300

    educacion

--- ProyectoIntegrador_DavalosAlejandro/Frontend/src/app/app-routing.module.ts
+++ ProyectoIntegrador_DavalosAlejandro/Frontend/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+import { NeweducacionComponent } from './components/educacion/neweducacion.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { HomeComponent } from './components/home/home.component';
@@ -12,7 +14,11 @@ const routes: Routes = [
 
   {path:'nuevaexp', component: NewExperienciaComponent},
 
-  {path:'editexp/:id', component: EditExperienciaComponent}
+  {path:'editexp/:id', component: EditExperienciaComponent},
+
+  {path:'nuevaedu', component: NeweducacionComponent},
+
+  {path:'editedu/:id', component: EditeducacionComponent}
 ];
 
 @NgModule({
